1d7345de018919b749f957aef491263881ed0977
Luis Ramirez <146735904+LuisRamirezv@users.noreply.github.com>
2025-04-12 17:17:15 +0100

    Adding more questions

--- aws_cloud_practitioner_quiz.js
+++ aws_cloud_practitioner_quiz.js
@@ -3,6 +3,186 @@
 // QUESTIONS
 const questions = [
   {
+    question: "AWS allows users to manage their resources using a web based user interface. What is the name of this interface?",
+    options: ["AWS CLI", "AWS API", "AWS SDK", "AWS Management Console"],
+    answer: "AWS Management Console"
+  },
+  {
+    question: "Which of the following is an example of horizontal scaling in the AWS Cloud?",
+    options: [
+      "Replacing an existing EC2 instance with a larger, more powerful one.",
+      "Increasing the compute capacity of a single EC2 instance to address the growing demands of an application.",
+      "Adding more RAM capacity to an EC2 instance.",
+      "Adding more EC2 instances of the same size to handle an increase in traffic."
+    ],
+    answer: "Adding more EC2 instances of the same size to handle an increase in traffic."
+  },
+  {
+    question: "You have noticed that several critical Amazon EC2 instances have been terminated. Which of the following AWS services would help you determine who took this action?",
+    options: [
+      "Amazon Inspector",
+      "AWS CloudTrail",
+      "AWS Trusted Advisor",
+      "EC2 Instance Usage Report"
+    ],
+    answer: "AWS CloudTrail"
+  },
+  {
+    question: "Which of the below options are related to the reliability of AWS? (Choose TWO)",
+    options: [
+      "Applying the principle of least privilege to all AWS resources.",
+      "Automatically provisioning new resources to meet demand.",
+      "All AWS services are considered Global Services, and this design helps customers serve their international users.",
+      "Providing compensation to customers if issues occur.",
+      "Ability to recover quickly from failures."
+    ],
+    answer: "Automatically provisioning new resources to meet demand, Ability to recover quickly from failures"
+  },
+  {
+    question: "Which statement is true regarding the AWS Shared Responsibility Model?",
+    options: [
+      "Responsibilities vary depending on the services used.",
+      "Security of the IaaS services is the responsibility of AWS.",
+      "Patching the guest OS is always the responsibility of AWS.",
+      "Security of the managed services is the responsibility of the customer."
+    ],
+    answer: "Responsibilities vary depending on the services used."
+  },
+  {
+    question: "You have set up consolidated billing for several AWS accounts. One of the accounts has purchased a number of reserved instances for 3 years. Which of the following is true regarding this scenario?",
+    options: [
+      "The Reserved Instance discounts can only be shared with the master account.",
+      "All accounts can receive the hourly cost benefit of the Reserved Instances.",
+      "The purchased instances will have better performance than On-demand instances.",
+      "There are no cost benefits from using consolidated billing; It is for informational purposes only."
+    ],
+    answer: "All accounts can receive the hourly cost benefit of the Reserved Instances."
+  },
+  {
+    question: "A company has developed an eCommerce web application in AWS. What should they do to ensure that the application has the highest level of availability?",
+    options: [
+      "Deploy the application across multiple Availability Zones and Edge locations.",
+      "Deploy the application across multiple Availability Zones and subnets.",
+      "Deploy the application across multiple Regions and Availability Zones.",
+      "Deploy the application across multiple VPC’s and subnets."
+    ],
+    answer: "Deploy the application across multiple Regions and Availability Zones."
+  },
+  {
+    question: "What does AWS Snowball provide? (Choose TWO)",
+    options: [
+      "Built-in computing capabilities that allow customers to process data locally.",
+      "A catalog of third-party software solutions that customers need to build solutions and run their businesses.",
+      "A hybrid cloud storage between on-premises environments and the AWS Cloud.",
+      "An Exabyte-scale data transfer service that allows you to move extremely large amounts of data to AWS.",
+      "Secure transfer of large amounts of data into and out of the AWS."
+    ],
+    answer: "Built-in computing capabilities that allow customers to process data locally, Secure transfer of large amounts of data into and out of the AWS."
+  },
+  {
+    question: "A company has an AWS Enterprise Support plan. They want quick and efficient guidance with their billing and account inquiries. Which of the following should the company use?",
+    options: [
+      "AWS Health Dashboard",
+      "AWS Support Concierge",
+      "AWS Customer Service",
+      "AWS Operations Support"
+    ],
+    answer: "AWS Support Concierge"
+  },
+  {
+    question: "A Japanese company hosts their applications on Amazon EC2 instances in the Tokyo Region. The company has opened new branches in the United States, and the US users are complaining of high latency. What can the company do to reduce latency for the users in the US while minimizing costs?",
+    options: [
+      "Applying the Amazon Connect latency-based routing policy.",
+      "Registering a new US domain name to serve the users in the US.",
+      "Building a new data center in the US and implementing a hybrid model.",
+      "Deploying new Amazon EC2 instances in a Region located in the US."
+    ],
+    answer: "Deploying new Amazon EC2 instances in a Region located in the US."
+  },
+  {
+    question: "An organization has a large number of technical employees who operate their AWS Cloud infrastructure. What does AWS provide to help organize them into teams and then assign the appropriate permissions for each team?",
+    options: [
+      "IAM roles",
+      "IAM users",
+      "IAM user groups",
+      "AWS Organizations"
+    ],
+    answer: "IAM user groups"
+  },
+  {
+    question: "A company has decided to migrate its Oracle database to AWS. Which AWS service can help achieve this without negatively impacting the functionality of the source database?",
+    options: [
+      "AWS OpsWorks",
+      "AWS Database Migration Service",
+      "AWS Server Migration Service",
+      "AWS Application Discovery Service"
+    ],
+    answer: "AWS Database Migration Service"
+  },
+  {
+    question: "Adjusting compute capacity dynamically to reduce cost is an implementation of which AWS cloud best practice?",
+    options: [
+      "Build security in every layer.",
+      "Parallelize tasks.",
+      "Implement elasticity.",
+      "Adopt monolithic architecture."
+    ],
+    answer: "Implement elasticity."
+  },
+  {
+    question: "What are the benefits of having infrastructure hosted in AWS? (Choose TWO)",
+    options: [
+      "Increasing speed and agility.",
+      "There is no need to worry about security.",
+      "Gaining complete control over the physical infrastructure.",
+      "Operating applications on behalf of customers.",
+      "All of the physical security and most of the data/network security are taken care of for you."
+    ],
+    answer: "Increasing speed and agility, All of the physical security and most of the data/network security are taken care of for you."
+  },
+  {
+    question: "What is the advantage of the AWS-recommended practice of 'decoupling' applications?",
+    options: [
+      "Allows treating an application as a single, cohesive unit.",
+      "Reduces inter-dependencies so that failures do not impact other components of the application.",
+      "Allows updates of any monolithic application quickly and easily.",
+      "Allows tracking of any API call made to any AWS service."
+    ],
+    answer: "Reduces inter-dependencies so that failures do not impact other components of the application."
+  },
+  {
+    question: "Which of the following helps a customer view the Amazon EC2 billing activity for the past month?",
+    options: [
+      "AWS Budgets",
+      "AWS Pricing Calculator",
+      "AWS Systems Manager",
+      "AWS Cost & Usage Reports"
+    ],
+    answer: "AWS Cost & Usage Reports"
+  },
+  {
+    question: "What do you gain from setting up consolidated billing for five different AWS accounts under another master account?",
+    options: [
+      "AWS services’ costs will be reduced to half the original price.",
+      "The consolidated billing feature is just for organizational purpose.",
+      "Each AWS account gets volume discounts.",
+      "Each AWS account gets five times the free-tier services capacity."
+    ],
+    answer: "Each AWS account gets volume discounts."
+  },
+  {
+    question: "What should you do in order to keep the data on EBS volumes safe? (Choose TWO)",
+    options: [
+      "Regularly update firmware on EBS devices.",
+      "Create EBS snapshots.",
+      "Ensure that EBS data is encrypted at rest.",
+      "Store a backup daily in an external drive.",
+      "Prevent any unauthorized access to AWS data centers."
+    ],
+    answer: "Create EBS snapshots, Ensure that EBS data is encrypted at rest."
+  },
+
+  {
     question: "Which AWS service is used to run code without provisioning or managing servers?",
     options: ["EC2", "Lambda", "ECS", "Lightsail"],
     answer: "Lambda"
